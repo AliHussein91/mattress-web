db0218b3fa7fd1f5dcc7896c2a9426df8de43200
Ali Hussein <ali.hussein@live.com>
2024-09-16 04:22:42 +0300

    Header created, translation added & linked to language change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shell/header/header.component";
 import { FooterComponent } from "./shell/footer/footer.component";
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'mattress-web';
+export class AppComponent implements OnInit {
+  direction: 'rtl' | 'ltr' = 'rtl'
+  private translateService = inject(TranslateService);
+  ngOnInit(): void {
+    const defaultLange = localStorage.getItem('language') || 'ar';
+    this.translateService.setDefaultLang(defaultLange)
+    this.translateService.use(defaultLange)
+  }
+
+  changeLang(lang: 'en' | 'ar') {
+    this.translateService.setDefaultLang(lang)
+    this.translateService.use(lang)
+    localStorage.setItem('language', lang)
+    lang === 'en' ? this.direction = 'ltr' : this.direction = 'rtl'
+  }
 }
--- src/app/shell/header/header.component.ts
+++ src/app/shell/header/header.component.ts
@@ -1,12 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  @Input() promo: string = "- سجل الأن واحصل علي هديتك خصة 10% استخدم كود Mattress10 -"
+  lang: string = 'ar'
+  langImg: string = '../../../assets/img/ar.png'
+  @Input() promo: string = "register now and get 10% discount using voucher Mattress10"
+  @Output() langChange = new EventEmitter()
+
+
+  changeLanguageEmitter(event: Event) {
+    const lang = (event.target as HTMLSelectElement).value
+    this.lang = lang;
+    lang == 'ar' ? this.langImg = "../../../assets/img/ar.png" : this.langImg = "../../../assets/img/en.png"
+    this.langChange.emit(lang)
+  }
 }
