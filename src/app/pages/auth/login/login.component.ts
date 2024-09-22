e46d920a67322bd406b9fda48298c0b382005233
Ali Hussein <ali.hussein@live.com>
2024-09-22 16:15:41 +0300

    Created login form

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SimpleHeaderComponent } from "../../../shell/components/simple-header/simple-header.component";
+import { TranslateModule } from '@ngx-translate/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [SimpleHeaderComponent, TranslateModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  isVisible = false
+  passType = 'password'
+
+  fb = inject(FormBuilder)
+  loginForm = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/)]],
+    stayIn: [false]
+  })
+
+
+  onSubmit() {
+    console.log(this.loginForm.getRawValue())
+  }
+
+  showPassword() {
+    this.isVisible = !this.isVisible
+    this.isVisible? this.passType = 'text' : this.passType = 'password'
+  }
+  // Stay In Functionality Here
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#192c72",
+        secondary: "#5e78c9",
+        orange: "#eab755",
+        black: "#2e2e2e",
+        blackLight: "#383838",
+        grey: "#78787a",
+        offWhite: "#f7f5f0",
+        white: "#fff",
+        background: "#f9f9f9",
+      },
+    },
   },
   plugins: [],
-}
-
+};
