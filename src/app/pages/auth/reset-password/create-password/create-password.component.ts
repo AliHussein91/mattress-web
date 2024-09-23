4ec8820edaa3c5af07def06fe7095865419855f3
Ali Hussein <ali.hussein@live.com>
2024-09-23 22:39:35 +0300

    Create Password Component created

--- src/app/pages/auth/reset-password/create-password/create-password.component.ts
+++ src/app/pages/auth/reset-password/create-password/create-password.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SimpleHeaderComponent } from "../../../../shell/components/simple-header/simple-header.component";
+import { TranslateModule } from '@ngx-translate/core';
+import { passwordValidator } from '../../validators/password.validator';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-password',
   standalone: true,
-  imports: [],
+  imports: [SimpleHeaderComponent, TranslateModule, ReactiveFormsModule],
   templateUrl: './create-password.component.html',
   styleUrl: './create-password.component.scss'
 })
 export class CreatePasswordComponent {
+  onSubmit() {
+    console.log(this.newPassForm.getRawValue().password);
+  }
+  isVisible = false
+  isConVisible = false
+  passType = 'password'
+  confType = 'password'
 
+  fb = inject(FormBuilder)
+  newPassForm = this.fb.nonNullable.group({
+    password: ['', [Validators.required, passwordValidator()]],
+    confirmation: ['', [Validators.required, passwordValidator()]],
+  })
+
+  showPassword() {
+    this.isVisible = !this.isVisible
+    this.isVisible ? this.passType = 'text' : this.passType = 'password'
+  }
+  showConfirmation() {
+    this.isConVisible = !this.isConVisible
+    this.isConVisible ? this.confType = 'text' : this.confType = 'password'
+  }
 }
