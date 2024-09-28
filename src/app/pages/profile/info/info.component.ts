815ef48a283dbb270c57c9f42ab1dea339c932c3
Ali Hussein <ali.hussein@live.com>
2024-09-28 16:22:36 +0300

    Merging with master

--- src/app/pages/profile/info/info.component.ts
+++ src/app/pages/profile/info/info.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { AvatarInputComponent } from '../../../shell/components/avatar-input/avatar-input.component';
+import { PhoneInputComponent } from '../../../shell/components/phone-input/phone-input.component';
+import { InputComponent } from '../../../shell/components/input/input.component';
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, AvatarInputComponent, PhoneInputComponent, InputComponent],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss'
 })
