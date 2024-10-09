4a840ca32fa4274b2344bf2f4a3738b337533e09
Ali Hussein <ali.hussein@live.com>
2024-10-09 11:45:43 +0300

    Updated signup and added g-map for user to pick location

---
+++ src/app/shared/components/g-map/geocode.ts
@@ -0,0 +1,61 @@
+export interface geocode {
+    "plus_code": {
+        "compound_code": string,
+        "global_code": string,
+    },
+    "results": [
+        {
+            "formatted_address": string,
+            "types": []
+        },
+        {
+            "formatted_address": string,
+            "types": []
+        },
+        {
+            "formatted_address": string,
+            "types": []
+        },
+        {
+            "formatted_address": string,
+            "types": []
+        },
+        {
+            "formatted_address": string,
+            "types": []
+        },
+        {
+            "formatted_address": string,
+            "types": []
+        },
+        {
+            "formatted_address": string,
+            "types": []
+        },
+        {
+            "formatted_address": string,
+            "types": []
+        },
+        {
+            "formatted_address": string,
+            "types": []
+        },
+        {
+            "formatted_address": string,
+            "types": []
+        },
+        {
+            "formatted_address": string,
+            "types": []
+        },
+        {
+            "formatted_address": string,
+            "types": []
+        },
+        {
+            "formatted_address": string,
+            "types": []
+        }
+    ],
+    "status": string,
+}
