77420d15c582eb6567f96897d8bf5ed9f87ef9a5
MidoHussei1993 <m.hussin1993@gmail.com>
2024-10-14 20:25:37 +0300

    Add SweetAlert2 for confirmation dialogs and update translations
    - Introduced SwalModalService for handling confirmation modals.
    - Added new translations for confirmation prompts in Arabic and English.
    - Updated product details and cart components to utilize the new confirmation service.
    - Included SweetAlert2 in package dependencies.

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,26 +1,40 @@
 import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { NumberRestricDirective } from '../../shared/directives/numbers-only.directive';
 import { CartFacade } from '@app/core/state/cart/facade';
-import { ICart } from '@app/shared/types';
+import { CartProduct, ICart, Product } from '@app/shared/types';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { ActionsUtilties } from '@app/shared/util';
+import { HttpClient } from '@angular/common/http';
+import { SwalModalService } from '@app/shared/services';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [TranslateModule, RouterLink, NumberRestricDirective,FormsModule],
+  imports: [
+    TranslateModule,
+    RouterLink,
+    NumberRestricDirective,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
-export class CartComponent extends ActionsUtilties implements OnInit,OnDestroy {
-
+export class CartComponent
+  extends ActionsUtilties
+  implements OnInit, OnDestroy
+{
   protected cartFacade = inject(CartFacade);
+  protected http = inject(HttpClient);
+  #swal = inject(SwalModalService);
+  #translate = inject(TranslateService);
   protected cart: ICart = {} as ICart;
   private subscription: Subscription = new Subscription();
+  busyDeletingProduct: boolean = false;
 
   ngOnInit(): void {
     this.subscription = this.cartFacade.cart$.subscribe((res) => {
@@ -28,11 +42,39 @@ export class CartComponent extends ActionsUtilties implements OnInit,OnDestroy {
       console.log('🚀 ~ HeaderComponent ~ ngOnInit ~ this.cart:', this.cart);
     });
   }
- 
+
   amount = 2500;
   reward = 'free delivery';
   value = signal('76%');
   totalPrice = 2800;
+
+  deleteProduct(product: CartProduct) {
+    this.#swal.Confirmation(this.#translate.instant('areYouSure'),this.#translate.instant('deleteProduct')).then((res) => {
+      this.busyDeletingProduct = true;
+      this.http
+        .post(
+          this.getAction(product, 'delete_product_from_cart').endpoint_url,
+          {},
+        )
+        .subscribe({
+          next: (value) => {
+            console.log(
+              '🚀 ~ ProductCardComponent ~ this.http.post ~ value',
+              value,
+            );
+          },
+          error: (err) => {
+            console.log(
+              '🚀 ~ ProductCardComponent ~ this.http.post ~ err:',
+              err,
+            );
+          },
+          complete: () => {
+            this.busyDeletingProduct = false;
+          },
+        });
+    });
+  }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
---
+++ src/app/shared/services/swal-modal.service.ts
@@ -0,0 +1,112 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import Swal, { SweetAlertResult } from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SwalModalService {
+  constructor(private translate: TranslateService) {}
+  // public showDeleteConfirmation(elementName?): Promise<SweetAlertResult> {
+  //   elementName ? (elementName = `"${elementName}"`) : (elementName = '');
+
+  //   return new Promise((resolve, reject) => {
+  //     Swal.fire({
+  //       title: 'هل انت متأكد؟',
+  //       text: `حذف العنصر ${elementName}!`,
+  //       icon: 'warning',
+  //       showCancelButton: true,
+  //       confirmButtonText: 'نعم, احذف!',
+  //       cancelButtonText: 'إلغاء',
+  //     }).then((result) => {
+  //       resolve(result);
+  //     });
+  //   });
+  // }
+  async deleteConfirmation(): Promise<boolean> {
+    return await Swal.fire({
+      title: 'هل انت متاكد',
+      text: 'حذف العنصر',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'نعم احذف',
+      cancelButtonText: 'إلغاء',
+    }).then((result) => {
+      return result.isConfirmed;
+    });
+  }
+
+  async Confirmation(
+    title?: string ,
+    text?:string,
+    acceptButtonText: string=this.translate.instant('yes'),
+  ): Promise<boolean> {
+    return await Swal.fire({
+      ...(title && { title: this.translate.instant(title) }),
+      ...(text && { html: text }),
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: acceptButtonText,
+      cancelButtonText:this.translate.instant('no')
+    }).then((result) => {
+      return result.isConfirmed;
+    });
+  }
+
+  Notifier(title: string) {
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: title,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+  watched(title: string) {
+    Swal.fire({
+      position: 'center',
+      icon: 'info',
+      title: title,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+  confirm(text: string) {
+    Swal.fire({
+      icon: 'success',
+      position: 'center',
+      title: 'للعلم',
+      html: text,
+      // footer: '<a href>Why do I have this issue?</a>'
+    });
+  }
+  NotifierError(text: string) {
+    Swal.fire({
+      position: 'center',
+      icon: 'error',
+      title: 'خطاء ...',
+      text: text,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+
+  async navigationConfirmation(): Promise<boolean> {
+    return await Swal.fire({
+      title: 'هل انت متاكد',
+      text: 'الانتقال دون حفظ التغيرات',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'نعم الانتقال',
+      cancelButtonText: 'إلغاء',
+    }).then((result) => {
+      return result.isConfirmed;
+    });
+  }
+}
